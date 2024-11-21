88fe42b83ffdeaba3d62f59d643dc558fd73b1e2
Rizqi Alif Permana Putra <141630316+RizqiAlif09@users.noreply.github.com>
2024-11-21 07:37:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def predict():
         return render_template('aplikasi.html', hasil_prediksi=None, nilai_kepercayaan=None, error_text=error_text)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
